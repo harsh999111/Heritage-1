55a588ae2c440b755cae74569e9b5f039883535c
Harsh Baraiya <baraiyaharsh319@gmail.com>
2024-02-23 12:57:39 +0530

    place upadated

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Place from '../../components/Place/place'
 import Carsole from '../../components/Carosel/carosel'
 import Place1 from '../../components/Place1/Place'
-import Contact from '../../components/Contact/Contact'
-import Navbar from '../../components/Navbar/navbar';
-import Footer from '../../components/Footer/footer';
 
 const Home = () => {
   return (
