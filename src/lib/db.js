db5e07c71f114da8cf4ff2c1ba9facdf954f88fd
Harsh Baraiya <baraiyaharsh319@gmail.com>
2024-03-15 11:23:29 +0530

    code upadated

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,5 @@
 const { username, password } = process.env;
 export const connectionSrt = 
-"mongodb://"+username+":"+password+"@ac-tejbxn5-shard-00-00.whakfbh.mongodb.net:27017,ac-tejbxn5-shard-00-01.whakfbh.mongodb.net:27017,ac-tejbxn5-shard-00-02.whakfbh.mongodb.net:27017/?ssl=true&replicaSet=atlas-6fdqpf-shard-0&authSource=admin&retryWrites=true&w=majority"
+"mongodb://"+username+":"+password+"@cluster0.mdse8y1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // "mongodb://"+username+":"+password+"@ac-tejbxn5-shard-00-00.whakfbh.mongodb.net:27017,ac-tejbxn5-shard-00-01.whakfbh.mongodb.net:27017,ac-tejbxn5-shard-00-02.whakfbh.mongodb.net:27017/?ssl=true&replicaSet=atlas-6fdqpf-shard-0&authSource=admin&retryWrites=true&w=majority"
+
